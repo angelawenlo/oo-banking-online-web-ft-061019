74d7ffcb856b29715c2e91b21524bd24a24f2d6c
Angela Lo <angelawenlo@gmail.com>
2019-06-27 17:28:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.balance > 0 && @status = "pending" 
+    if sender.balance > amount && @status = "pending" 
     @sender.balance -= amount
     @receiver.balance += amount
     @status = "complete"
